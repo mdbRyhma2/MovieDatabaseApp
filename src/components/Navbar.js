31da85c6cee3bde35506bb00ed192e19c722439b
t3roja <ronkainen@hotmail.fi>
2024-11-23 15:26:20 +0200

    Improvements to genre and relaseyear criteria

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,26 @@ import { useUser } from "../context/useUser";
 
 export default function Navbar() {
   const { user, setUser } = useUser(); // Accessing user data and the setter function
-  const [searchParam, setSearchParam] = useState("");
+  const [searchParam, setSearchParam] = useState('');
   const navigate = useNavigate();
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     if (searchParam.trim()) {
+      // Navigate to Search.js with the query
       navigate(`/search?query=${searchParam}`);
     }
   };
 
-  // Function for logout
+  const handleAdvancedSearch = () => {
+    // Navigate to Search.js with no prefilled parameters
+    navigate('/search');
+  };
+
   const handleLogout = () => {
-    sessionStorage.removeItem("user");
-    setUser({ email: "", password: "" });
-    navigate("/");
+    sessionStorage.removeItem('user');
+    setUser({ email: '', password: '' });
+    navigate('/');
   };
 
   return (
@@ -29,8 +34,6 @@ export default function Navbar() {
         <a className="navbar-brand" href="#">
           Heading
         </a>
-
-
         <div className="navbar-desktop" id="navbarSupportedContent">
           <ul className="navbar-links">
             <li className="nav-item">
@@ -48,35 +51,27 @@ export default function Navbar() {
                 Showtimes
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Extra?
-              </Link>
-            </li>
             <form onSubmit={handleSearchSubmit}>
               <input
                 type="text"
                 placeholder="Search..."
                 value={searchParam}
                 onChange={(e) => setSearchParam(e.target.value)}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    handleSearchSubmit(e);
-                  }
-                }}
               />
+              <button type="submit">Search</button>
             </form>
-                <li className="nav-item">
-                   <button className='navbar-button' onClick={() => navigate('/search')}>Advanced search</button>
-                </li>
-            {/* Button for login and logout*/}
+            <li className="nav-item">
+              <button className="navbar-button" onClick={handleAdvancedSearch}>
+                Advanced Search
+              </button>
+            </li>
             <li className="nav-item">
               {user.token ? (
                 <button className="navbar-button" onClick={handleLogout}>
                   Log Out
                 </button>
               ) : (
-                <button className="navbar-button" onClick={() => navigate("/login")}>
+                <button className="navbar-button" onClick={() => navigate('/login')}>
                   Login
                 </button>
               )}
