a6977d3157321e1f160bd114023f8b875fa7dc8e
t3roja <t3roja@students.oamk.fi>
2024-11-27 20:20:59 +0200

    MovieDetails page added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Search from './pages/Search';
 import Groups from './pages/Groups';
 import Showtimes from './pages/Showtimes'
-import Movies from './pages/Movies';
+import Group from './pages/Group';
 import Authentication, { AuthenticationMode } from "./pages/Authentication";
+import MovieDetails from './pages/MovieDetails';
 
 export default function App() {
     return (
-    <div style={{ paddingTop: '6rem' }}>
-        <Navbar />
-        <Header />
-        
-        <Routes>
-            <Route path='/' exact element={<Home />} />
-            <Route path="/showtimes" element={<Showtimes />} />
-            <Route path="/groups" element={<Groups />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/login" element={<Authentication authenticationMode={AuthenticationMode.Login} />} />
-            <Route path="/signup" element={<Authentication authenticationMode={AuthenticationMode.Register} />} />
-        </Routes>
+        <div style={{ paddingTop: '6rem' }}>
+            <Navbar />
+            <Header />
+
+            <Routes>
+                <Route path='/' exact element={<Home />} />
+                <Route path="/showtimes" element={<Showtimes />} />
+                <Route path="/groups" element={<Groups />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/group" element={<Group />} />
+                <Route path="/movie/:id" element={<MovieDetails />} />
+                <Route path="/login" element={<Authentication authenticationMode={AuthenticationMode.Login} />} />
+                <Route path="/signup" element={<Authentication authenticationMode={AuthenticationMode.Register} />} />
+            </Routes>
 
 
-        
-    </div>
+
+        </div>
     )
 }
