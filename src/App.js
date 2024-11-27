8f554e7dee4eb20ffc79bfe0f029c1508478894d
oona123 <oona.saarikoski@hotmail.com>
2024-11-27 11:00:23 +0200

    ryhmä lista valmis joista pääsee ryhmien profiiliin, tietokantaa ei ole vielä yhdistetty

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Search from './pages/Search';
 import Groups from './pages/Groups';
+import Group from './pages/Group';
 import Showtimes from './pages/Showtimes'
 import Movies from './pages/Movies';
 import Authentication, { AuthenticationMode } from "./pages/Authentication";
@@ -19,6 +20,7 @@ export default function App() {
             <Route path='/' exact element={<Home />} />
             <Route path="/showtimes" element={<Showtimes />} />
             <Route path="/groups" element={<Groups />} />
+            <Route path="/group/:id" element={<Group />} />
             <Route path="/search" element={<Search />} />
             <Route path="/login" element={<Authentication authenticationMode={AuthenticationMode.Login} />} />
             <Route path="/signup" element={<Authentication authenticationMode={AuthenticationMode.Register} />} />
--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 export default function Groups() {
 
-    return (
-        <div id="container">
-          <h3>Groups list</h3>
+  const [group, setgroup] = useState([
+    { id: 1, name: 'Group 1' },
+    { id: 2, name: 'Group 2' },
+    { id: 3, name: 'Group 3' },
+    { id: 4, name: 'Group 4' },
+    { id: 5, name: 'Group 5' }
+  ]);
+
+  return (
+    <div id="container">
+      <section className="groups">
+        <h2>Groups</h2>
+        <div className="group-list">
+          <ul>
+            {group.map((groupItem) => (
+              <li key={group.id}>
+                <Link to={`/group/${groupItem.id}`}>{groupItem.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      )
-    }
+      </section>
+    </div>
+  );
+}
