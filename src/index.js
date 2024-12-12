73dec8f6855ec3bbdb302c7fc2a737c00ce42f97
SallaPR <salla.pr@hotmail.com>
2024-12-12 22:12:24 +0200

    navbar done

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import UserProvider from "./context/UserProvider";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import App from './App';
 
