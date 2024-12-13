5a6a24888ce7468a7412771ab6311ec6d738b802
SallaPR <salla.pr@hotmail.com>
2024-12-13 16:01:28 +0200

    edited group pages

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -18,26 +18,20 @@ export default function Group({}) {
   });
   const navigate = useNavigate();
 
-
-  const defaultImage = "https://via.placeholder.com/50";
-
-  //GROUP
-
-  //fetch group by id
+  // Fetch group information by id
   useEffect(() => {
     const fetchGroupById = async () => {
       try {
         const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/group/group/${id}`
         );
-        // fetch group_name and owner_id from database
+        // Fetch group_name and owner_id from database
         const groupName = response.data.rows[0].group_name;
-
         const ownerId = response.data.rows[0].owner_id;
         setGroupName(groupName);
         setOwnerId(ownerId);
 
-        //check if you are owner. Only owner can delete group and groupmembers
+        // Check if the current user is the owner
         if (ownerId === user.id) {
           setIsOwner(true);
         }
@@ -46,53 +40,49 @@ export default function Group({}) {
       }
     };
 
-
+    // Get group's movies
     const fetchGroupMovies = async () => {
       try {
         const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/group/getGroupMovies/` + id
         );
 
-        setMovies(response.data)
-
-
+        setMovies(response.data);
       } catch (error) {
         console.error("Error fetching group movies:", error);
       }
-    }
+    };
 
+    // Fetch group members
     const fetchGroupMembers = async () => {
       try {
         const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/group/groupmember/${id}`
         );
-        const groupMembers = response.data;
+        const groupMembers = response.data; // Members list
         setMembers(groupMembers);
-        //check if u are member if u are not u can join group
-        const isMember = groupMembers.some(
-          (member) => member.username === user.username
-        );
+
+        // Check if the current user is a member
+        const isMember = groupMembers.some((member) => member.username === user.username);
         setIsMember(isMember);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchGroupById();
+    fetchGroupById(); // Fetch group details
 
-    // Is the user token present get group movies else empty the list
-    if (user.token){
+    // Fetch data if user is authenticated
+    if (user.token) {
       fetchGroupMembers();
       fetchGroupMovies();
     } else {
-      setMovies([])
-      setMembers([])
+      setMovies([]);
+      setMembers([]);
     }
-
-
   }, [id, user.id]);
 
-  //delete group
+  // Delete group and navigate back to the groups list
   const deleteGroupPage = async () => {
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this group and all its data?"
@@ -105,22 +95,19 @@ export default function Group({}) {
         `${process.env.REACT_APP_API_URL}/group/group/${id}`
       );
       alert("Group deleted successfully");
-      //navigate back to groups page
-      navigate("/groups");
+      navigate("/groups"); // Navigate back to groups page
     } catch (error) {
       console.error("Error deleting group:", error);
       alert("Failed to delete group.");
     }
   };
 
-  //GROUPMEMBERS
-
+  // Save members to localStorage whenever members state changes
   useEffect(() => {
     localStorage.setItem("members", JSON.stringify(members));
   }, [members]);
 
-
-  //join group
+  // Join the group
   const joinGroup = async () => {
     try {
       const response = await axios.post(
@@ -130,16 +117,15 @@ export default function Group({}) {
           group_id: id,
         }
       );
-      //add user to members array
-      setMembers((prevMembers) => [...prevMembers, user]);
+      setMembers((prevMembers) => [...prevMembers, user]); // Add user to member list
       alert("You joined group", groupName);
-      setIsMember(true);
+      setIsMember(true); // Update member status
     } catch (error) {
       console.error("Error creating group:", error);
     }
   };
 
-  //delete groupMember or leave a group
+  // Leave a group or delete a member
   const deleteGroupMember = async () => {
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this groupmember?"
@@ -161,42 +147,42 @@ export default function Group({}) {
       setMembers((prevMembers) =>
         prevMembers.filter((member) => member.username !== user.username)
       );
-      setIsMember(false);
+      setIsMember(false); // Update member status
     } catch (error) {
       console.error("Error deleting groupmember:", error);
       alert("Failed to delete groupmember.");
     }
   };
 
+  // Remove a movie from the group 
   const handleRemoveFromGroupMoviesClick = async (movie_id) => {
-
     try {
-      await axios.delete(process.env.REACT_APP_API_URL + '/groups/removeFromUserMovies',{
+      await axios.delete(process.env.REACT_APP_API_URL + "/groups/removeFromUserMovies", {
         data: {
-        groupId : id,
-        movieId : movie_id
-        }}
-      )
+          groupId: id,
+          movieId: movie_id,
+        },
+      });
 
-        setMovies((prevMovies) =>
-          prevMovies.filter((movie) => movie.movie_id !== movie_id)
-        ) 
+      setMovies((prevMovies) =>
+        prevMovies.filter((movie) => movie.movie_id !== movie_id)
+      );
     } catch (error) {
-      console.error("Failed to remove movie from group list: ", error.status)
+      console.error("Failed to remove movie from group list: ", error.status);
     }
-
-  }
+  };
 
   return (
-    <div className="container">
+    <div className="group-container">
       <header className="header">
-        <img src={defaultImage} alt="Group Default" className="group-image" />
         <div className="group-name">
           <h1>{groupName}</h1>
         </div>
         <div className="actions">
           <p>
-            {isMember
+            {isOwner
+              ? "You are the owner of the group"
+              : isMember
               ? "You are a member of the group"
               : "You are not a member of the group"}
           </p>
@@ -206,55 +192,59 @@ export default function Group({}) {
             <button onClick={joinGroup}>Join Group</button>
           )}
 
-          {isOwner ? (
-            <button onClick={deleteGroupPage}>Delete group</button>
-          ) : (
-            ""
-          )}
+          {isOwner ? <button onClick={deleteGroupPage}>Delete group</button> : ""}
         </div>
       </header>
+      {isMember ? (
+        <section className="members">
+          <h2>Group members</h2>
+          <div className="members-list">
+            <ul>
+              {members.map((member) => (
+                <li key={member.user_id}>
+                  {member.username}
+                  {member.role}
+                  {isOwner && (
+                    <button onClick={() => deleteGroupMember(member.user_id)}>
+                      Delete user from group
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
 
-      <section className="members">
-        <h2>Group members</h2>
-        <div className="members-list">
-          <ul>
-            {members.map((member) => (
-              <li key={member.user_id}>
-                {member.username}
-                {member.role}
-                {isOwner && (
-                  <button onClick={() => deleteGroupMember(member.user_id)}>
-                    Delete user from group
-                  </button>
+      {isMember ? (
+        <section className="movies">
+          <h2>Movies</h2>
+          <div className="movies-grid">
+            {movies.map((movie) => (
+              <li key={movie.movie_id} className="movie-card">
+                {movie.poster_path ? (
+                  <Link to={`/movie/${movie.movie_id}`}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  </Link>
+                ) : (
+                  <p>No Image Available</p>
                 )}
+                <Link to={`/movie/${movie.movie_id}`}>{movie.movie_title}</Link>
+                <button onClick={() => handleRemoveFromGroupMoviesClick(movie.movie_id)}>
+                  Remove
+                </button>
               </li>
             ))}
-          </ul>
-        </div>
-      </section>
-
-      <section className="movies">
-  <h2>Movies</h2>
-  <div className="movies-grid">
-    {movies.map((movie) => (
-          <li key={movie.movie_id} className="movie-card">
-            {movie.poster_path ? (
-              <Link to={`/movie/${movie.movie_id}`}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                  alt={movie.title}
-                />
-              </Link>
-            ) : (
-              <p>No Image Available</p>
-            )}
-            <Link to={`/movie/${movie.movie_id}`}>{movie.movie_title}</Link>
-            <button onClick={() => handleRemoveFromGroupMoviesClick(movie.movie_id)}>Remove</button>
-          </li>
-    ))}
-  </div>
-</section>
-
+          </div>
+        </section>
+      ) : (
+        <p>You need to join the group to see the group page</p>
+      )}
     </div>
   );
 }
